f746a5b152b8cc6da71223f447e0d2019efd3936
crazy010323 <crazy010323@gmail.com>
2022-05-20 14:34:14 -0400

    redeem NFT

--- test/Redeem-NFT.js
+++ test/Redeem-NFT.js
@@ -11,6 +11,29 @@ redeemNFT()
 
  */
 
+const compareTwoObjects = (obj1, obj2) => {
+  if (!obj1 && !obj2) return true;
+  if (!obj1) return false;
+  if (!obj2) return false;
+
+  const key1 = Object.keys(obj1).filter(
+    (key) => parseInt(key).toString() !== key
+  );
+  const key2 = Object.keys(obj2).filter(
+    (key) => parseInt(key).toString() !== key
+  );
+
+  if (key1.length !== key2.length) return false;
+  for (let i = 0; i < key1.length; i++) {
+    const k1 = key1[i];
+    const k2 = key2[i];
+    if (k1 !== k2) return false;
+    // if (obj1[k1].toString() !== obj2[k2].toString()) return false;
+    if (parseInt(obj1[k1], 16) !== parseInt(obj2[k2], 16)) return false;
+  }
+  return true;
+};
+
 describe("Terminate rent agreement and reset lending metadata", async () => {
   const NFT_ADDRESS = "0xF8764D543ae563A0B42761DCd31bE102603b722E"; // Smol Runners
   const NFT_NAME = "SmolRunners";
@@ -116,57 +139,61 @@ describe("Terminate rent agreement and reset lending metadata", async () => {
       const currentOwner = await SmolRunnersNFT.ownerOf(ORIGINAL_NFT_ID);
       expect(currentOwner).to.equal(owner.address);
     });
+    it("Metadata for rTokenId should be reset", async () => {
+      expect(
+        await rNFT.getRnftFromNft(NFT_ADDRESS, owner.address, ORIGINAL_NFT_ID)
+      ).to.equal(0);
+    });
   });
-  // describe("Gateway/terminateRentAgreement : Terminate rent agreement & clear rent metadata", async () => {
-  //   beforeEach(async () => {
-  //     // first of all, needs to list for lending
-  //     await gateway.createLendRecord(
-  //       NFT_ADDRESS,
-  //       ORIGINAL_NFT_ID,
-  //       MAX_DURATION * ONE_MONTH,
-  //       MIN_DURATION * ONE_MONTH,
-  //       ONE_MONTH,
-  //       RENT_PRICE_PER_TIMEUNIT_ETH,
-  //       ETH_ADDRESS
-  //     );
-  //     // approve & premint
-  //     await gateway.approveAndPreMintRNFT(
-  //       NFT_ADDRESS,
-  //       ORIGINAL_NFT_ID,
-  //       MAX_DURATION * ONE_MONTH,
-  //       renter.address
-  //     );
-  //     // get RTokenId
-  //     rTokenId = await rNFT.getRnftFromNft(
-  //       NFT_ADDRESS,
-  //       owner.address,
-  //       ORIGINAL_NFT_ID
-  //     );
-  //     // confirm payment
-  //     await gateway
-  //       .connect(renter)
-  //       .confirmRentAgreementAndPay(NFT_ADDRESS, ORIGINAL_NFT_ID, {
-  //         value: RENT_PRICE_PER_TIMEUNIT_ETH * MAX_DURATION,
-  //       });
-  //   });
-  //   it("Should revert with message 'unauthorized: address is not owner or lending not registered' unless caller's the lender", async () => {
-  //     await expect(
-  //       gateway
-  //         .connect(other)
-  //         .terminateRentAgreement(NFT_ADDRESS, ORIGINAL_NFT_ID)
-  //     ).to.be.revertedWith(
-  //       "unauthorized: address is not owner or lending not registered"
-  //     );
-  //   });
-  //   it("Success : Should emit the event 'Rent_Agreemeng_Terminated'", async () => {
-  //     // stimulate time
-  //     await ethers.provider.send("evm_increaseTime", [
-  //       ONE_MONTH * MAX_DURATION,
-  //     ]);
-  //     // check
-  //     await expect(gateway.terminateRentAgreement(NFT_ADDRESS, ORIGINAL_NFT_ID))
-  //       .to.emit(gateway, "Rent_Agreemeng_Terminated")
-  //       .withArgs(NFT_ADDRESS, ORIGINAL_NFT_ID, rTokenId);
-  //   });
-  // });
+  describe("Gateway/redeemNFT : redeems NFT from listing/lending & takes ownership back", async () => {
+    it("Should revert with message 'unauthorized: address is not owner or lending not registered' when an annonymous accounts calls", async () => {
+      await expect(
+        gateway.connect(other).redeemNFT(NFT_ADDRESS, ORIGINAL_NFT_ID)
+      ).to.be.revertedWith(
+        "unauthorized: address is not owner or lending not registered"
+      );
+
+      // stimulate time
+      await ethers.provider.send("evm_increaseTime", [
+        ONE_MONTH * MAX_DURATION,
+      ]);
+      await ethers.provider.send("evm_mine");
+      // redeem
+      await gateway.redeemNFT(NFT_ADDRESS, ORIGINAL_NFT_ID);
+    });
+    it("Should revert with message 'RNFT Token ID doesn't exist' for an pre-minted NFT", async () => {
+      await gateway.createLendRecord(
+        NFT_ADDRESS,
+        ORIGINAL_NFT_ID + 1,
+        MAX_DURATION * ONE_MONTH,
+        MIN_DURATION * ONE_MONTH,
+        ONE_MONTH,
+        RENT_PRICE_PER_TIMEUNIT_ETH,
+        ETH_ADDRESS
+      );
+
+      await expect(
+        gateway.redeemNFT(NFT_ADDRESS, ORIGINAL_NFT_ID + 1)
+      ).to.be.revertedWith("RNFT Token ID doesn't exist");
+
+      // stimulate time
+      await ethers.provider.send("evm_increaseTime", [
+        ONE_MONTH * MAX_DURATION,
+      ]);
+      await ethers.provider.send("evm_mine");
+      // redeem
+      await gateway.redeemNFT(NFT_ADDRESS, ORIGINAL_NFT_ID);
+      // remove lending
+      await gateway.removeLending(NFT_ADDRESS, ORIGINAL_NFT_ID + 1);
+    });
+    it("Success : redeems NFT from listing", async () => {
+      // stimulate time
+      await ethers.provider.send("evm_increaseTime", [
+        ONE_MONTH * MAX_DURATION,
+      ]);
+      await ethers.provider.send("evm_mine");
+      // redeem
+      await gateway.redeemNFT(NFT_ADDRESS, ORIGINAL_NFT_ID);
+    });
+  });
 });
